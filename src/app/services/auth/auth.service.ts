b94eed1a680275d06a5ce7cb2b660c2bcf8a1534
omri ike <omri@email.com>
2024-10-30 12:25:20 +0200

    auth

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { Observable, Subject } from 'rxjs';
+
+export interface User {
+  _id: string;
+  fullName: string;
+  phone: string;
+  email: string;
+  isVerified: boolean;
+  paymentBank: string;
+  paymentNumber: string;
+  paymentBranch: string;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  userInfo = new Subject<User>();
+
+  constructor(private http: HttpClient) {
+  }
+  
+    
+  login(email:string, password:string ):Observable<{access_token:string}> {
+      return this.http.post<{access_token:string}>(`${environment.apiUrl}/auth/login`, {email, password})
+  }
+
+  getUserInfo() {
+    this.http.get<User>(`${environment.apiUrl}/auth/info`).subscribe((user)=>this.userInfo.next(user))
+  }
+  
+  setSession(authResult:any):void {
+      var t = new Date();
+      t.setSeconds(t.getSeconds() + 20);
+      const expiresAt = t;
+
+      localStorage.setItem('id_token', authResult.access_token);
+      localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+  }     
+  getAuthToken() {
+    return localStorage.getItem("id_token");
+  }     
+  // getUserData() {
+  //   return JSON.parse(localStorage.getItem("user_data"));
+  // }
+
+  logout() {
+      localStorage.removeItem("id_token");
+      localStorage.removeItem("expires_at");
+  }
+
+  public isLoggedIn() {
+      return this.getAuthToken() ? true : false; //moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+      return !this.isLoggedIn();
+  }
+
+  // getExpiration() {
+  //     const expiration = localStorage.getItem("expires_at");
+  //     const expiresAt = JSON.parse(expiration);
+  //     return moment(expiresAt);
+  // }    
+}
+       
